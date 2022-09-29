4d443c8a4fe64eea0231cbf58e8a2a1cffb928ac
Rodolfo Rodrigues Pizzo <109025291+rodpizzo@users.noreply.github.com>
2022-09-29 10:46:13 -0300

    Add files via upload

---
+++ exercices/A007 Body mass index (BMI).py
@@ -0,0 +1,11 @@
+# ENTRADA:
+a = float(input())
+b = float(input())
+
+# PROCESSAMENTO:
+# ...
+
+imc = (a/(b*b))
+
+# SAIDA:
+print(f"{imc:.2f}")
---
+++ exercices/A008 Hypotenuse.py
@@ -0,0 +1,12 @@
+# ENTRADA:
+import math
+a = float(input())
+b = float(input())
+
+# PROCESSAMENTO:
+# ...
+
+hip = math.sqrt((a*a)+(b*b))
+
+# SAIDA:
+print(f"{hip:.2f}")
